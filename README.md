4a7b388aa87b83625a230998f8db5d3d8b290832
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2024-08-09 19:47:19 +0300

    working nordpool

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Awtrix display apps
 
+* Nordpool homeassistant: https://flows.blueforcer.de/flow/gLB25zlMoWZp
+
+## Configuration file config.yml
+
+```yaml
+broker_address: <IP ADDRESS>
+user: <MQTT USER>
+password: <MQTT USER PASSWORD>
+homeassistant_api_url: <HOMEASSISTANT API URL>
+homeassistant_api_token: <HOMEASSISTANT BEARER TOKEN>
+```
+
 ## Send message to MQTT Broker using command line
 
 ```
 $ mosquitto_pub -h <broker_address> -t <topic> -m <message> -u <username> -P <password>
 ```
+
--- src/nordpool.py
+++ src/nordpool.py
@@ -6,40 +6,32 @@ def send_nordpool(broker_address, topic, user, password, homeassistant_api_url, 
     # Context variables
     nordpool_response = src.homeassistant.get_data_from_homeassistant_api(homeassistant_api_url, homeassistant_api_token, "sensor.nordpool_kwh_fi_eur_3_00_024")
     nordpool_prices = nordpool_response.json()
-    current_price = nordpool_prices.get("state")
+    current_price = float(nordpool_prices.get("state"))
     hourly_attribute = nordpool_prices.get("attributes").get("today")
+    nordpool_prices = hourly_attribute
     unit = "c/kWh"
     unit_multiplier = 100
     multiplied_price = current_price * unit_multiplier
-    color_matrix_json = {
-        "0": "#00FF00",
-        "20": "#FFFF00",
-        "40": "#FF0000"
-    }
-    color_dict = [(key, value) for key, value in color_matrix_json.items()]
     message_duration = 30
     icon = 54077
-    
+    price_text = str(multiplied_price) + unit
     # Jinja2
     template_dir = './files'  # Directory containing your template files
     env = Environment(loader=FileSystemLoader(template_dir))
     template_name = 'nordpool.json.j2'  # Replace with your template file name
     template = env.get_template(template_name)
-    print(template)
     context = {
         "hourly_attribute": hourly_attribute,
         "unit": unit,
         "unit_multiplier": unit_multiplier,
-        "color_matrix_json": color_matrix_json,
-        "color_dict": color_dict,
         "message_duration": message_duration,
         "icon": icon,
         "nordpool_prices": nordpool_prices,
         "current_price": current_price,
         "multiplied_price": multiplied_price,
-        "price_text": multiplied_price
+        "price_text": price_text
         }
     message = template.render(context)
     print(message)
-    #src.mqtt_message.send_mqtt_message(broker_address=str(broker_address), topic=str(topic), message=str(message), user=user, password=password)
+    src.mqtt_message.send_mqtt_message(broker_address=str(broker_address), topic=str(topic), message=str(message), user=user, password=password)
     return True
