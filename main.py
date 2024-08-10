af525cd5eeb89c450db3e9b9a095953e8a91d5f0
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2024-08-10 09:23:19 +0300

    update main

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ password = config['password']
 homeassistant_api_url = config['homeassistant_api_url']
 homeassistant_api_token = config['homeassistant_api_token']
 
-#src.mqtt_message.send_mqtt_clear_display(broker_address, user=user, password=password)
+src.mqtt_message.send_mqtt_clear_display(broker_address, user=user, password=password)
 #src.date.send_date(broker_address, topic="awtrix_1e7e38/custom/date", user=user, password=password)
 src.nameday.send_nameday_message(broker_address, topic="awtrix_1e7e38/custom/nameday", user=user, password=password)
 src.sun.send_sunrise(broker_address, topic="awtrix_1e7e38/custom/sunrise", user=user, password=password, homeassistant_api_url=homeassistant_api_url, homeassistant_api_token=homeassistant_api_token)
 src.sun.send_sunset(broker_address, topic="awtrix_1e7e38/custom/sunset", user=user, password=password, homeassistant_api_url=homeassistant_api_url, homeassistant_api_token=homeassistant_api_token)
 src.nordpool.send_nordpool(broker_address, topic="awtrix_1e7e38/custom/nordpool", user=user, password=password, homeassistant_api_url=homeassistant_api_url, homeassistant_api_token=homeassistant_api_token)
-src.weather.send_weather(broker_address, topic="awtrix_1e7e38/custom/weather", user=user, password=password, homeassistant_api_url=homeassistant_api_url, homeassistant_api_token=homeassistant_api_token)
+#src.weather.send_weather(broker_address, topic="awtrix_1e7e38/custom/weather", user=user, password=password, homeassistant_api_url=homeassistant_api_url, homeassistant_api_token=homeassistant_api_token)
