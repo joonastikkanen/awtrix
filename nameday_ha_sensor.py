af350ffc0caf2904a521a6d98c3f7a521fad1ed3
Joonas Tikkanen <joonas.tikkanen91@gmail.com>
2024-08-12 10:23:16 +0300

    add csv file path variable

--- nameday_ha_sensor.py
+++ nameday_ha_sensor.py
@@ -1,12 +1,23 @@
 import src.nameday
 import datetime
+import yaml
 
-def get_nameday():
+# LOAD CONFIG FILE
+def load_config():
+    with open('./config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+# Set variables from config file
+config = load_config()
+nameday_csv_path = config['nameday_csv_path']
+
+def get_nameday(nameday_csv_path):
     # Set day and month variables
     now = datetime.datetime.now()
     day = now.strftime("%d")
     month = now.strftime("%m")
-    file_path = './files/namedays.csv'
+    file_path = nameday_csv_path
     nameday = src.nameday.find_nameday(file_path, day, month)
     print(nameday)
     return nameday
